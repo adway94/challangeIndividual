d3ac1db59132b4c4be2849db309445ba91abb18d
adway94 <alejandromachulsky_1o@hotmail.com>
2021-02-23 16:58:22 -0300

    directory and files creation

---
+++ backend/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+
+const apiRoutes = require("./routes/apiRoutes");
+
+const { urlencoded } = require("body-parser");
+const port = process.env.PORT ? process.env.PORT : 3000;
+
+// Manejo de parmetros url y json
+app.use(urlencoded());
+app.use(express.json());
+
+// Prueba de servidor en index
+app.get("/", (req, res) => {
+  res.send("Im alive at port " + port);
+});
+
+// Agregamos rutas API
+app.use(apiRoutes);
+
+// Escucha puerto
+app.listen(port, () => {
+  console.log("Im alive at port: " + port);
+});
